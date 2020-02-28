fb6d1c7506a157c4f9cf184a5089e2a2760ce7cd
Vered Shwartz <vered1986@gmail.com>
2020-02-27 22:13:18 -0800

    Comet data dir as an argument

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools.command.develop import develop
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 SRC_DIR = os.path.join(ROOT_DIR, 'comet2')
 BUILD_DIR = SRC_DIR if os.path.exists(SRC_DIR) else os.path.join(ROOT_DIR, '../..')
+DATA_DIR = os.path.expanduser("~/.comet-data")
 
 
 with open("README.md", "r") as fh:
@@ -18,22 +19,30 @@ with open("README.md", "r") as fh:
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
+    user_options = [
+        ('data_dir=', DATA_DIR, f'Specify the data directory (default {DATA_DIR})'),
+    ]
+
     def run(self):
         install.run(self)
-        self.execute(_post_install, (), msg="Running post install task")
+        self.execute(_post_install, (self.data_dir,), msg="Running post install task")
 
 
 class PostDevelopCommand(develop):
     """Post-installation for development mode."""
+    user_options = [
+        ('data_dir=', DATA_DIR, f'Specify the data directory (default {DATA_DIR})'),
+    ]
+
     def run(self):
         develop.run(self)
-        self.execute(_post_install, (), msg="Running post install task")
+        self.execute(_post_install, (self.data_dir,), msg="Running post install task")
 
 
-def _post_install():
+def _post_install(data_dir):
     script = os.path.join(BUILD_DIR, os.pardir, "setup", "download.sh")
     subprocess.call(["chmod", "+x", script])
-    subprocess.call(["/bin/bash", "-c", script])
+    subprocess.call(["/bin/bash", "-c", script, data_dir])
 
 
 setuptools.setup(
--- setup/download.sh
+++ setup/download.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
+DATA_DIR=$1
+
 # Get ATOMIC data
-echo "mkdir -p ~/.comet2-data/data/atomic"
-mkdir -p ~/.comet-data/data/atomic;
-echo "wget https://homes.cs.washington.edu/~msap/atomic/data/atomic_data.tgz -O ~/.comet2-data/data/atomic/atomic_data.tgz"
+echo "mkdir -p ${DATA_DIR}/data/atomic"
+mkdir -p ${DATA_DIR}/data/atomic;
+echo "wget https://homes.cs.washington.edu/~msap/atomic/data/atomic_data.tgz -O ${DATA_DIR}/data/atomic/atomic_data.tgz"
 wget https://homes.cs.washington.edu/~msap/atomic/data/atomic_data.tgz;
-echo "tar -xvzf atomic_data.tgz -C ~/.comet2-data/data/atomic/"
-tar -xvzf atomic_data.tgz -C ~/.comet-data/data/atomic/;
+echo "tar -xvzf atomic_data.tgz -C ${DATA_DIR}a/data/atomic/"
+tar -xvzf atomic_data.tgz -C ${DATA_DIR}/data/atomic/;
 echo "rm atomic/atomic_data.tgz"
 rm atomic_data.tgz;
 
 # Get Pre-trained COMET model
-echo "mkdir -p ~/.comet2-data/models/"
-mkdir -p ~/.comet-data/models/;
+echo "mkdir -p ${DATA_DIR}/models/"
+mkdir -p ${DATA_DIR}/models/;
 echo "gdown https://drive.google.com/uc?id=1aIIoTz9m28yyW1ygg6OG-JkAVLUyuIRh"
 gdown https://drive.google.com/uc?id=1aIIoTz9m28yyW1ygg6OG-JkAVLUyuIRh;
-echo "unzip atomic_pretrained_model.zip -d ~/.comet2-data/models/"
-unzip atomic_pretrained_model.zip -d ~/.comet-data/models/;
+echo "unzip atomic_pretrained_model.zip -d ${DATA_DIR}/models/"
+unzip atomic_pretrained_model.zip -d ${DATA_DIR}/models/;
 echo "rm atomic_pretrained_model.zip"
 rm atomic_pretrained_model.zip;
-echo "mv ~/.comet2-data/models/atomic_pretrained_model ~/.comet2-data/models/atomic_pretrained_model_openai-gpt"
-mv ~/.comet-data/models/atomic_pretrained_model ~/.comet-data/models/atomic_pretrained_model_openai-gpt;
+echo "mv ${DATA_DIR}/models/atomic_pretrained_model ${DATA_DIR}/models/atomic_pretrained_model_openai-gpt"
+mv ${DATA_DIR}/models/atomic_pretrained_model ${DATA_DIR}/models/atomic_pretrained_model_openai-gpt;
