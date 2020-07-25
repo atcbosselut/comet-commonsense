0042875b79af18a5b30c502613bd4a832cb47627
Antoine Bosselut <atcbosselut@users.noreply.github.com>
2020-07-25 15:43:09 -0700

    Update functions.py

--- src/interactive/functions.py
+++ src/interactive/functions.py
@@ -4,6 +4,8 @@ from src.data.utils import TextEncoder
 import src.data.config as cfg
 import src.data.data as data
 import src.models.models as models
+import src.train.batch as batch_utils
+
 from src.evaluate.sampler import BeamSampler, GreedySampler, TopKSampler
 
 import utils.utils as utils
@@ -229,6 +231,91 @@ def set_conceptnet_inputs(input_event, relation, text_encoder, max_e1, max_r, fo
     batch["attention_mask"] = data.conceptnet_data.make_attention_mask(XMB)
 
     return batch, abort
+
+def set_conceptnet_inputs_for_evaluation(input_event, relation, output,
+                                         text_encoder, max_e1, max_r, max_e2):
+    abort = False
+
+    e1_tokens, rel_tokens, e2_tokens = data.conceptnet_data.do_example(
+        text_encoder, input_event, relation, output)
+
+    if len(e2_tokens) > max_e2:
+        return {}, True
+    elif len(e1_tokens) > max_e1:
+        return {}, True
+    else:
+        XMB = torch.zeros(
+            1, max_e1 + max_r + len(e2_tokens)).long().to(cfg.device)
+
+    XMB[:, :len(e1_tokens)] = torch.LongTensor(e1_tokens)
+    XMB[:, max_e1:max_e1 + len(rel_tokens)] = torch.LongTensor(rel_tokens)
+    XMB[:, max_e1 + max_r:] = torch.LongTensor(e2_tokens)
+
+    batch = {}
+    batch["sequences"] = XMB
+    batch["attention_mask"] = data.conceptnet_data.make_attention_mask(XMB)
+    batch["loss_mask"] = data.conceptnet_data.make_loss_mask(
+        XMB, max_e1 + max_r)
+
+    return batch, abort
+
+def evaluate_conceptnet_sequence(e1, model, data_loader,
+                                 text_encoder, relation, e2, force=False):
+    if isinstance(relation, list):
+        outputs = {}
+
+        for rel in relation:
+            new_outputs = evaluate_conceptnet_sequence(
+                e1, model, data_loader, text_encoder, rel, e2)
+            outputs.update(new_outputs)
+        return outputs
+    elif relation == "all":
+        outputs = {}
+
+        for relation in data.conceptnet_data.conceptnet_relations:
+            new_outputs = evaluate_conceptnet_sequence(
+                e1, model, data_loader, text_encoder, relation, e2)
+            outputs.update(new_outputs)
+        return outputs
+    else:
+
+        sequence_all = {}
+
+        sequence_all["e1"] = e1
+        sequence_all["relation"] = relation
+        sequence_all["e2"] = e2
+
+        with torch.no_grad():
+            if data_loader.max_r != 1:
+                relation_sequence = data.conceptnet_data.split_into_words[relation]
+            else:
+                relation_sequence = "<{}>".format(relation)
+
+            batch, abort = set_conceptnet_inputs_for_evaluation(
+                e1, relation_sequence, e2, text_encoder,
+                data_loader.max_e1, data_loader.max_r, data_loader.max_e2)
+
+            if abort:
+                return {relation: sequence_all}
+
+            input_ = batch_utils.model_utils.prepare_position_embeddings(
+                None, data_loader.vocab_encoder, batch["sequences"].unsqueeze(-1))
+            attention_mask = batch["attention_mask"]
+            loss_mask = batch["loss_mask"]
+
+            targets = input_[:, 1:, 0].contiguous().view(-1)
+
+            loss, dist = batch_utils.mle_steps(
+                "", model, input_[:, :-1, :], targets,
+                attention_mask[:, :-1], loss_reduction="none")
+
+            final_loss = (loss * loss_mask)
+            
+        sequence_all['total_loss'] = final_loss.sum().item()
+        sequence_all['normalized_loss'] = (final_loss.sum() / loss_mask.sum()).item()
+        sequence_all['step_losses'] = final_loss[0, data_loader.max_e1 + data_loader.max_r - 1:].tolist()
+
+        return {relation: sequence_all}
 
 
 def print_conceptnet_sequence(sequence_object):
